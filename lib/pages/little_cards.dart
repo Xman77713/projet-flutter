02cb099c53c74cca9dc60ed0c41ebb59b3297aef
Vince-lgd <lagardevincent64@gmail.com>
2024-12-05 17:19:54 +0100

    WIP

--- lib/pages/little_cards.dart
+++ lib/pages/little_cards.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_projet_final/res/app_colors.dart';
 
 class LittleCards extends StatelessWidget {
   const LittleCards({super.key});
@@ -8,26 +9,32 @@ class LittleCards extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: SizedBox(
-          width: 200,
-          height: 250,
+          width: 180,
+          height: 242,
           child: Card(
-            color: Colors.blueGrey,
+            color: AppColors.green,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(10),
             ),
-            child: const Column(
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 ClipRRect(
-                  borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(8),
+                  borderRadius: const BorderRadius.vertical(
+                    top: Radius.circular(10),
+                  ),
+                  child: Image.network(
+                    'https://i0.wp.com/www.filmspourenfants.net/wp-content/uploads/2018/10/titans-a.jpg?fit=333%2C446&ssl=1',
+                    height: 177,
+                    width: 180,
+                    fit: BoxFit.cover,
                   ),
                 ),
-                Padding(
+                const Padding(
                   padding: EdgeInsets.all(8.0),
                   child: Text(
                     'Titans',
-                    style: TextStyle(color: Colors.white),
+                    style: TextStyle(color: Colors.white, fontSize: 16),
                   ),
                 ),
               ],
