d8312d17a00f99435a74b2c323fd03b3a0db0991
axelmensonge <axel.veri@gmail.com>
2025-01-11 16:10:40 +0100

    feat: 3ème test API

---
+++ lib/api/model/responseAPIImageURL.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'responseAPIImageURL.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ImageURL _$ImageURLFromJson(Map<String, dynamic> json) => ImageURL(
+      json['small_url'] as String?,
+    );
+
+Map<String, dynamic> _$ImageURLToJson(ImageURL instance) => <String, dynamic>{
+      'small_url': instance.smallUrl,
+    };
