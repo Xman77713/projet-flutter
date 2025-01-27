849578de349ab0312e91910e42e351b9e0d9f0bb
Vince-lgd <lagardevincent64@gmail.com>
2025-01-27 19:40:38 +0100

    correctif text in card to liste and add blur in image details

--- lib/interface/pages/first_bloc_details_movies.dart
+++ lib/interface/pages/first_bloc_details_movies.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:ui'; // Pour le flou
+
 import 'package:flutter_projet_final/interface/pages/tabDetailMovie/second_bloc_details_movies_infos.dart';
 import 'package:flutter_projet_final/interface/pages/tabDetailMovie/second_bloc_details_movies_people.dart';
 import 'package:flutter_projet_final/interface/pages/tabDetailMovie/second_bloc_details_movies_synopsis.dart';
-import 'package:flutter_projet_final/interface/pages/tabHomePage/list_movies.dart';
 import 'package:flutter_projet_final/res/app_colors.dart';
 
 class FirstBlocDetailsMovies extends StatelessWidget {
@@ -15,119 +16,145 @@ class FirstBlocDetailsMovies extends StatelessWidget {
     return DefaultTabController(
       length: 3,
       child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 2, 8, 14),
-          title: const Row(
-            children: [
-              SizedBox(width: 8),
-              Text(
-                'Watchmen',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
+        body: Stack(
+          children: [
+            Positioned.fill(
+              child: Image.network(
+                'https://i0.wp.com/www.filmspourenfants.net/wp-content/uploads/2018/10/titans-a.jpg?fit=333%2C446&ssl=1',
+                fit: BoxFit.cover,
+              ),
+            ),
+
+            //TO DO Vérifier si c'est correct
+            Positioned.fill(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                child: Container(
+                  color: Colors.black.withOpacity(0.4),
                 ),
               ),
-            ],
-          ),
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(12),
-                    child: Image.network(
-                      'https://i0.wp.com/www.filmspourenfants.net/wp-content/uploads/2018/10/titans-a.jpg?fit=333%2C446&ssl=1',
-                      height: 127,
-                      width: 95,
-                      fit: BoxFit.cover,
-                    ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: AppColors.seeMoreBackground.withOpacity(0.5),
                   ),
-                  const SizedBox(width: 16),
-                  const Column(
+                  padding: const EdgeInsets.only(top: 40, left: 16, right: 16),
+                  child: const Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(Icons.arrow_back, color: Colors.white),
+                      SizedBox(width: 8),
+                      Text(
+                        'Watchmen',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Row(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      SizedBox(height: 40),
-                      Row(
-                        children: [
-                          Icon(
-                            Icons.theaters,
-                            color: Colors.white,
-                            size: 17,
-                          ),
-                          SizedBox(width: 6),
-                          Text(
-                            '162 minutes',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 14,
-                            ),
-                          ),
-                        ],
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(12),
+                        child: Image.network(
+                          'https://i0.wp.com/www.filmspourenfants.net/wp-content/uploads/2018/10/titans-a.jpg?fit=333%2C446&ssl=1',
+                          height: 127,
+                          width: 95,
+                          fit: BoxFit.cover,
+                        ),
                       ),
-                      SizedBox(height: 8),
-                      Row(
+                      const SizedBox(width: 16),
+                      const Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Icon(
-                            Icons.calendar_today,
-                            color: Colors.white,
-                            size: 17,
+                          SizedBox(height: 40),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.theaters,
+                                color: Colors.white,
+                                size: 17,
+                              ),
+                              SizedBox(width: 6),
+                              Text(
+                                '162 minutes',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
                           ),
-                          SizedBox(width: 6),
-                          Text(
-                            '2009',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 14,
-                            ),
+                          SizedBox(height: 8),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.calendar_today,
+                                color: Colors.white,
+                                size: 17,
+                              ),
+                              SizedBox(width: 6),
+                              Text(
+                                '2009',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
                     ],
                   ),
-                ],
-              ),
-            ),
-            const TabBar(
-              tabs: <Widget>[
-                SizedBox(
-                  width: 120,
-                  child: Tab(
-                    text: 'Synopsis',
-                  ),
                 ),
-                SizedBox(
-                  width: 120,
-                  child: Tab(
-                    text: 'Personnages',
-                  ),
+                const TabBar(
+                  tabs: <Widget>[
+                    SizedBox(
+                      width: 120,
+                      child: Tab(
+                        text: 'Synopsis',
+                      ),
+                    ),
+                    SizedBox(
+                      width: 120,
+                      child: Tab(
+                        text: 'Personnages',
+                      ),
+                    ),
+                    SizedBox(
+                      width: 120,
+                      child: Tab(
+                        text: 'Infos',
+                      ),
+                    ),
+                  ],
+                  indicatorColor: AppColors.orange,
+                  labelColor: Colors.white,
+                  unselectedLabelColor: AppColors.bottomBarUnselectedText,
+                  indicatorWeight: 6.0,
+                  labelStyle: TextStyle(fontSize: 17),
                 ),
-                SizedBox(
-                  width: 120,
-                  child: Tab(
-                    text: 'Infos',
+                const Expanded(
+                  child: TabBarView(
+                    children: <Widget>[
+                      SecondBlocDetailsMoviesSynopsis(),
+                      SecondBlocDetailsMoviesPeople(),
+                      SecondBlocDetailsMoviesInfos(),
+                    ],
                   ),
                 ),
               ],
-              indicatorColor: AppColors.orange,
-              labelColor: Colors.white,
-              unselectedLabelColor: AppColors.bottomBarUnselectedText,
-              indicatorWeight: 6.0,
-              labelStyle: TextStyle(fontSize: 17),
-            ),
-            const Expanded(
-              child: TabBarView(
-                children: <Widget>[
-                  SecondBlocDetailsMoviesSynopsis(),
-                  SecondBlocDetailsMoviesPeople(),
-                  SecondBlocDetailsMoviesInfos(),
-                ],
-              ),
             ),
           ],
         ),
