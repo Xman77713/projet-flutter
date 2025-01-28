93dae2c46cbbf42fe3eed859a7d1a54655810f4e
axelmensonge <axel.veri@gmail.com>
2025-01-28 22:49:16 +0100

    feat: couleur synopsis + scrollbar

--- lib/interface/pages/tabDetailMovie/second_bloc_details_movies_synopsis.dart
+++ lib/interface/pages/tabDetailMovie/second_bloc_details_movies_synopsis.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 
+import '../../../res/app_colors.dart';
+
 class SecondBlocDetailsMoviesSynopsis extends StatelessWidget {
   final String? description;
 
@@ -19,13 +21,17 @@ class SecondBlocDetailsMoviesSynopsis extends StatelessWidget {
             borderRadius: BorderRadius.circular(20),
           ),
           child: Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Align(
-                alignment: Alignment.topLeft,
-                child: HtmlWidget(description ?? 'Description indisponible')
-                //TODO scroll bar à ajouter et couleur à changer
+              padding: const EdgeInsets.all(15.0),
+              child: SingleChildScrollView(
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: HtmlWidget(description ?? 'Description indisponible',
+                      textStyle: const TextStyle(
+                        fontSize: 14,
+                        color: AppColors.bottomBarUnselectedText,
+                      )),
                 ),
-          ),
+              )),
         ),
       ),
     );
